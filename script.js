81dad982cf35697b12a9b289db5e23d010508e65
Luckey <luckeysherpa07@gmail.com>
2023-05-18 13:56:31 +0545

    added abcjs

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+// var visualOptions = {};
+// // var visualObj = ABCJS.renderAbc("paper", abcString, visualOptions);
+
+// // abcjs.renderAbc("paper", "X:1\nK:D\nDD AA|BBA2|\n");
+
+// // var abcString = "X:1\nT:Example\nK:Bb\nBcde|\n";
+// // renderAbc("target", abcString);
